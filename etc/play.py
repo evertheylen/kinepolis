8219456c4b26627af66363271910f5b6e0e518d5
Evert Heylen <evertheylen@gmail.com>
2014-11-15 23:23:03 +0100

    Star Wars!

---
+++ etc/play.py
@@ -0,0 +1,37 @@
+# Play the star wars movie!
+# By Evert Heylen
+
+import time
+
+def framePrint(frame):
+    print()
+    print('   ########################################################################   ')
+    print('   #                                                                      #   ')
+    
+    for l in frame:
+        print('   # {0: <68} #   '.format(l))
+    
+    print('   #                                                                      #   ')
+    print('   ########################################################################   ')
+    print()
+
+f = open('starwars.txt')
+
+line = 'START'
+
+while line != '':
+    pause = int(f.readline())
+    frame = []
+    for i in range(13):
+        line = f.readline()
+        frame.append(line[:len(line)-1])
+    
+    
+    framePrint(frame)
+    
+    for i in range(15):
+        print('\r\b\b\b\b\b\b\b\b\b\b\b\b\b\b',end='')
+    
+    time.sleep(0.01*pause)
+
+print('THE END')
