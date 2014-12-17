a56fcc8281d3fb47efb70b027cc0b89569cfbdd7
evertheylen <evertheylen@gmail.com>
2014-12-17 08:49:40 +0100

    rem klassen

--- kinepolis/klassen/ticket.py
+++
@@ -1,9 +0,0 @@
-class Ticket:
-    """ klasse voor een Ticket """
-    
-    def __init__(self, ID, reservatieID):
-        self.ID = ID
-        self.reservatieID = reservatieID
-        
-        # men kan de bijhorende gebruiker bereiken via de bijhorende reservatie.
-    
